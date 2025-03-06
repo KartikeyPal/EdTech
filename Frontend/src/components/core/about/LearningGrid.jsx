6f832579bca3b3b3af45ff64fbc9f8cc67d18b83
KartikeyPal <kartikeypal02@gmail.com>
2025-03-06 14:02:28 +0530

    Refactor About page components with improved styling and responsiveness

--- Frontend/src/components/core/about/LearningGrid.jsx
+++ Frontend/src/components/core/about/LearningGrid.jsx
@@ -2,80 +2,92 @@ import React from 'react'
 import HighlightText from '../homepage/HighlightText';
 import CTAButton from '../homepage/Button';
 const LearningGridArray = [
-    {
-      order: -1,
-      heading: "World-Class Learning for",
-      highlightText: "Anyone, Anywhere",
-      description:
-        "Studynotion partners with more than 275+ leading universities and companies to bring flexible, affordable, job-relevant online learning to individuals and organizations worldwide.",
-      BtnText: "Learn More",
-      BtnLink: "/",
-    },
-    {
-      order: 1,
-      heading: "Curriculum Based on Industry Needs",
-      description:
-        "Save time and money! The Belajar curriculum is made to be easier to understand and in line with industry needs.",
-    },
-    {
-      order: 2,
-      heading: "Our Learning Methods",
-      description:
-        "Studynotion partners with more than 275+ leading universities and companies to bring",
-    },
-    {
-      order: 3,
-      heading: "Certification",
-      description:
-        "Studynotion partners with more than 275+ leading universities and companies to bring",
-    },
-    {
-      order: 4,
-      heading: `Rating "Auto-grading"`,
-      description:
-        "Studynotion partners with more than 275+ leading universities and companies to bring",
-    },
-    {
-      order: 5,
-      heading: "Ready to Work",
-      description:
-        "Studynotion partners with more than 275+ leading universities and companies to bring",
-    },
-  ];
-  
+  {
+    order: -1,
+    heading: "World-Class Learning for",
+    highlightText: "Anyone, Anywhere",
+    description:
+      "Studynotion partners with more than 275+ leading universities and companies to bring flexible, affordable, job-relevant online learning to individuals and organizations worldwide.",
+    BtnText: "Learn More",
+    BtnLink: "/",
+  },
+  {
+    order: 1,
+    heading: "Curriculum Based on Industry Needs",
+    description:
+      "Save time and money! The Belajar curriculum is made to be easier to understand and in line with industry needs.",
+  },
+  {
+    order: 2,
+    heading: "Our Learning Methods",
+    description:
+      "Studynotion partners with more than 275+ leading universities and companies to bring",
+  },
+  {
+    order: 3,
+    heading: "Certification",
+    description:
+      "Studynotion partners with more than 275+ leading universities and companies to bring",
+  },
+  {
+    order: 4,
+    heading: `Rating "Auto-grading"`,
+    description:
+      "Studynotion partners with more than 275+ leading universities and companies to bring",
+  },
+  {
+    order: 5,
+    heading: "Ready to Work",
+    description:
+      "Studynotion partners with more than 275+ leading universities and companies to bring",
+  },
+];
+
 const LearningGrid = () => {
   return (
-    <div className='grid mx-auto grid-cols-1 lg:grid-cols-4 mb-10 '>
-        {
-            LearningGridArray.map((ele,ind)=>(
-                <div
-                key={ind}
-                className={`${ind===0 && ("lg:col-span-2 lg:h-[300px]")}  ${ele.order%2===1 ? ("bg-richblack-700"):("bg-richblack-800")} ${ele.order ===3 && "lg:col-start-2"}`}
-                >
-                        {
-                            ele.order<0 ? ( 
-                                <div className='lg: w-[90%] flex flex-col pb-5 gap-3'>
-                                    <div>
-                                        {ele.heading}
-                                        <HighlightText text={ele.highlightText}/>
-                                        <p>{ele.description}</p>
-                                        <CTAButton active={true} linkto={ele.BtnLink}>
-                                            {ele.BtnText}
-                                        </CTAButton>
-                                    </div>
-                                </div>
-                            ):(
-                                <div>
-                                    <h1>{ele.heading}</h1>
-                                    <p>{ele.description}</p>
-                                </div>
-                            )
-                        }
+    <div className="grid mx-auto w-[350px] xl:w-fit grid-cols-1 xl:grid-cols-4 mb-12">
+      {LearningGridArray.map((card, i) => {
+        return (
+          <div
+            key={i}
+            className={`${i === 0 && "xl:col-span-2 xl:h-[294px]"}  ${
+              card.order % 2 === 1
+                ? "bg-richblack-700 h-[294px]"
+                : card.order % 2 === 0
+                ? "bg-richblack-800 h-[294px]"
+                : "bg-transparent"
+            } ${card.order === 3 && "xl:col-start-2"}  `}
+          >
+            {card.order < 0 ? (
+              <div className="xl:w-[90%] flex flex-col gap-3 pb-10 xl:pb-0">
+                <div className="text-4xl font-semibold ">
+                  {card.heading}
+                  <HighlightText text={card.highlightText} />
                 </div>
-            ))
-        }
-    </div>
-  )
-}
+                <p className="text-richblack-300 font-medium">
+                  {card.description}
+                </p>
 
-export default LearningGrid
+                <div className="w-fit mt-2">
+                  <CTAButton active={true} linkto={card.BtnLink}>
+                    {card.BtnText}
+                  </CTAButton>
+                </div>
+              </div>
+            ) : (
+              <div className="p-8 flex flex-col gap-8">
+                <h1 className="text-richblack-5 text-lg">{card.heading}</h1>
+
+                <p className="text-richblack-300 font-medium">
+                  {card.description}
+                </p>
+              </div>
+            )}
+          </div>
+        );
+      })}
+    </div>
+  );
+};
+
+export default LearningGrid;
