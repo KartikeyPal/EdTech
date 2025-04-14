e00999339973ffff443d5f328f6bb629a14dbf19
KartikeyPal <kartikeypal02@gmail.com>
2025-04-14 16:11:28 +0530

    last commit

--- Backend/index.js
+++ Backend/index.js
@@ -23,7 +23,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
     cors({
-        origin:"https://cheery-crostata-b5a5ba.netlify.app",
+        origin:"https://edtech-glfo.onrender.com",
         credentials:true
     })
 );
